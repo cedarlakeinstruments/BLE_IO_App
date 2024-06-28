ca9fca7fd5a31b7d3d9523e5aaf11e5f4cbf4951
cedarlakeinstruments <lyndon@cedarlakeinstruments.com>
2024-06-27 21:01:50 -0500

    Output working.

--- CircuitPlaygroundApp/Controls.xaml.cs
+++ CircuitPlaygroundApp/Controls.xaml.cs
@@ -8,7 +8,14 @@ namespace CircuitPlaygroundApp;
 
 public partial class Controls : ContentPage
 {
-    public bool connected { get; private set; } = false;
+    bool connected = false;
+
+    string dataInServiceUuid = "4A98aaaa-1CC4-E7C1-C757-F1267DD021E8";
+    string dataInCharacteristicUuid = "11f836a0-5da3-4bba-815e-d55d2d1c08bc";
+
+    ICharacteristic writeCharacteristic;
+    string dataOutServiceUuid = "69137a96-521e-4f05-b3d2-446d68da158a";
+    string dataOutCharacteristicUuid = "506561bf-2208-4ffb-b0c1-f5e75bec35ad";
 
     public Controls()
     {
@@ -44,6 +51,8 @@ public partial class Controls : ContentPage
 
             try
             {
+                await adapter.StopScanningForDevicesAsync();
+
                 // May need to request location services also
                 StateLbl.Text = "Scanning...";
 
@@ -57,16 +66,21 @@ public partial class Controls : ContentPage
                 {
                     if (dev.Name.Contains("Playground"))
                     {
-                        //await adapter.StopScanningForDevicesAsync(dev);
 
                         DevicesLbl.Text = dev.Name;
+
+                        // Update connected state when it happens
+                        adapter.DeviceConnected += (s, a) => connected = true;
+
                         await adapter.ConnectToDeviceAsync(dev);
                         StateLbl.Text = "Connected";
 
-                        // "4A98aaaa-1CC4-E7C1-C757-F1267DD021E8" from Franklin code
-                        string guid = "11f836a0-5da3-4bba-815e-d55d2d1c08bc";
-                        var service = await dev.GetServiceAsync(Guid.Parse(guid));
-                        var characteristic = await service.GetCharacteristicAsync(Guid.Parse(guid));
+                        var service = await dev.GetServiceAsync(Guid.Parse(dataInServiceUuid));
+                        var characteristic = await service.GetCharacteristicAsync(Guid.Parse(dataInCharacteristicUuid));
+
+                        var writeService = await dev.GetServiceAsync(Guid.Parse(dataOutServiceUuid));
+                        writeCharacteristic = await writeService.GetCharacteristicAsync(Guid.Parse(dataOutCharacteristicUuid));
+
                         characteristic.ValueUpdated += (o, args) =>
                         {
                             MainThread.BeginInvokeOnMainThread(() =>
@@ -89,8 +103,18 @@ public partial class Controls : ContentPage
         }
 
     }
-    private async void OnScanClicked(object sender, EventArgs e)
+    private void OnActivateClicked(object sender, EventArgs e)
     {
-         Connect();
+        if (connected)
+        {
+            writeCharacteristic.WriteAsync(new byte[] { 65 });
+        }
+    }
+    private void OnDeactivateClicked(object sender, EventArgs e)
+    {
+        if (connected)
+        {
+            writeCharacteristic.WriteAsync(new byte[] { 66 });
+        }
     }
 }
--- PlaygroundDevice/code.py
+++ PlaygroundDevice/code.py
@@ -23,9 +23,9 @@ import adafruit_ble
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.uuid import VendorUUID
 from adafruit_ble.services import Service
-from adafruit_ble.characteristics import Characteristic
+from adafruit_ble.characteristics.int import Uint8Characteristic
 from adafruit_ble.characteristics.string import StringCharacteristic
-from adafruit_ble.characteristics.json import JSONCharacteristic
+from adafruit_ble.characteristics import Characteristic
 
 #############################################  U S E R  A D J U S T M E N T S #############################
 #
@@ -36,10 +36,17 @@ BRIGHTNESS = 0.7
 
 NUM_PIXELS = 10
 
+class ControlService(Service):
+    uuid = VendorUUID("69137a96-521e-4f05-b3d2-446d68da158a")
+    control = Uint8Characteristic(uuid = VendorUUID("506561bf-2208-4ffb-b0c1-f5e75bec35ad"), 
+    properties = Characteristic.WRITE | Characteristic.WRITE_NO_RESPONSE,)
+ 
+    def __init__(self, service=None):
+        super().__init__(service=service)
+        self.connectable = True
+        
 class SensorService(Service):
-
-    #uuid = VendorUUID("4A98aaaa-1CC4-E7C1-C757-F1267DD021E8")
-    uuid = VendorUUID("11f836a0-5da3-4bba-815e-d55d2d1c08bc")
+    uuid = VendorUUID("4A98aaaa-1CC4-E7C1-C757-F1267DD021E8")
     sensor = StringCharacteristic(
         uuid=VendorUUID("11f836a0-5da3-4bba-815e-d55d2d1c08bc"),
         properties = Characteristic.READ | Characteristic.NOTIFY,
@@ -66,13 +73,14 @@ pixels.fill((0,0,0))
 # Create radio
 ble = adafruit_ble.BLERadio()
 service = SensorService()
+controller = ControlService()
 
 # Advertising config
 myAdvertisement = ProvideServicesAdvertisement(service)
 myAdvertisement.connectable = True
 ble.name = "Playground"
 ble.start_advertising(myAdvertisement)
-
+ 
 # Show version
 print ("PlaygroundDevice v1.0")
 
@@ -83,4 +91,7 @@ while True:
     print ("Connected")
     while ble.connected:
         service.sensor = str(lightSensor.value) #{"light":lightSensor.value}
-        time.sleep(1.0)
+        print (controller.control) 
+        time.sleep(3.0)
+        
+    print ("Disconnected")
